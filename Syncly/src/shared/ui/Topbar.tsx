93f6bbadb20653d691723fd922990d12e9b46919
202110861 <dbsalsk2514@naver.com>
2025-04-27 10:17:21 +0900

    feat: Icon 컴포넌트 추가

---
+++ Syncly/src/shared/ui/Topbar.tsx
@@ -0,0 +1,16 @@
+import Icon from "./Icon";
+
+const Topbar = () => {
+  return (
+    <div>
+      <h1>Syncly</h1>
+      <div>
+        <Icon name="Bell" />
+        <Icon name="Vector" />
+        <Icon name="Log_Out" />
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
