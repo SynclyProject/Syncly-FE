fd9f941a95c05e355275727a962ee681dc90b8d0
202110861 <dbsalsk2514@naver.com>
2025-04-27 10:32:17 +0900

    feat: tailwind 설정

--- Syncly/tailwind.config.js
+++ Syncly/tailwind.config.js
@@ -6,13 +6,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        dongguk: "rgba(203, 96, 21, 1)", // #cb6015
-        "light-dongguk": "rgba(203, 96, 21, 0.4)", // cb6015 / 40
-        "typography-type-1": "#83786f; // [#83786f]",
-        "sul-gray-800": "#2E2E2E",
-        "sul-gray-600": "#636363",
-        "sul-gray-400": "#A6A6A6",
-        "sul-gray-200": "#DEDEDE",
+        "main-color": "#028090",
+        "sub-color-blue": "#456990",
       },
     },
   },
