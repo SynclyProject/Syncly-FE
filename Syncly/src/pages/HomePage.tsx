d91ecbae56ae767b82920c853d53a1534cab02f8
202110861 <dbsalsk2514@naver.com>
2025-04-30 14:41:18 +0900

    feat: Input 컴포넌트 테스트 코드 삭제

--- Syncly/src/pages/HomePage.tsx
+++ Syncly/src/pages/HomePage.tsx
@@ -1,26 +1,5 @@
-import Input from "../shared/ui/Input";
-
 const HomePage = () => {
-  return (
-    <div>
-      Homepage
-      <Input
-        title="Email"
-        placeholder="Enter your email address... "
-        state="default"
-      />
-      <Input
-        title="Email"
-        placeholder="Enter your email address... "
-        state="success"
-      />
-      <Input
-        title="Email"
-        placeholder="Enter your email address... "
-        state="failed"
-      />
-    </div>
-  );
+  return <div>Homepage</div>;
 };
 
 export default HomePage;
