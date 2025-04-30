b9e144b75afc0e576e379d2b27f3805fedf0539c
202110861 <dbsalsk2514@naver.com>
2025-04-30 16:59:48 +0900

    feat: 로그인 페이지인 경우 topbar 아이콘, sidebar 숨김

--- Syncly/src/Layout.tsx
+++ Syncly/src/Layout.tsx
@@ -1,13 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import TopBar from "./shared/ui/Topbar";
 import SideBar from "./shared/ui/Sidebar/Sidebar";
 
 const RootLayout = () => {
+  const location = useLocation();
+  const hideSidebar = location.pathname === "/login";
   return (
     <div className="flex flex-col">
       <TopBar />
       <div className="flex">
-        <SideBar />
+        {!hideSidebar && <SideBar />}
         <Outlet />
       </div>
     </div>
--- Syncly/src/shared/ui/Topbar.tsx
+++ Syncly/src/shared/ui/Topbar.tsx
@@ -1,20 +1,31 @@
+import { useLocation, useNavigate } from "react-router-dom";
 import Icon from "./Icon";
 
 const TopBar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const hideIcon = location.pathname === "/login";
   return (
     <div className="w-full h-[70px] bg-[#FFFFFF] px-[30px] flex justify-between items-center border border-[#E0E0E0]">
-      <p className="text-[20px] font-[600]">Syncly</p>
-      <div className="flex gap-[10px]">
-        <button className="bg-transparent border-none cursor-pointer">
-          <Icon name="Bell" />
-        </button>
-        <button className="bg-transparent border-none cursor-pointer">
-          <Icon name="User_Circle" />
-        </button>
-        <button className="bg-transparent border-none cursor-pointer">
-          <Icon name="Log_Out" />
-        </button>
-      </div>
+      <p
+        className="text-[20px] font-[600] cursor-pointer"
+        onClick={() => navigate("/")}
+      >
+        Syncly
+      </p>
+      {!hideIcon && (
+        <div className="flex gap-[10px]">
+          <button className="bg-transparent border-none cursor-pointer">
+            <Icon name="Bell" />
+          </button>
+          <button className="bg-transparent border-none cursor-pointer">
+            <Icon name="User_Circle" />
+          </button>
+          <button className="bg-transparent border-none cursor-pointer">
+            <Icon name="Log_Out" />
+          </button>
+        </div>
+      )}
     </div>
   );
 };
