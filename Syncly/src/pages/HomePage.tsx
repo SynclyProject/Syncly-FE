a22be6a5ca96bbb85a8c2da944f4798898500d5a
202110861 <dbsalsk2514@naver.com>
2025-05-07 15:57:20 +0900

    feat: file 컴포넌트

--- Syncly/src/pages/HomePage.tsx
+++ Syncly/src/pages/HomePage.tsx
@@ -1,10 +1,16 @@
 import Url from "../components/URLs/Url";
+import File from "../components/Files/File";
 
 const HomePage = () => {
   return (
     <div>
       <Url state="input" />
       <Url state="url" text="website.net" />
+      <File
+        type="folder"
+        title="textddddddddddddddddddddddddddddddd"
+        date="2025.05.05"
+      />
     </div>
   );
 };
