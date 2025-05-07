a6bcc5216120e76d7897e6dfdf0076da1a25333e
202110861 <dbsalsk2514@naver.com>
2025-05-07 16:54:40 +0900

    feat: My Space URLs 의 네비바

--- Syncly/src/pages/HomePage.tsx
+++ Syncly/src/pages/HomePage.tsx
@@ -1,16 +1,13 @@
-import Url from "../components/URLs/Url";
-import File from "../components/Files/File";
+import Navigate from "../components/navigate";
+import Button from "../shared/ui/Button";
 
 const HomePage = () => {
   return (
-    <div>
-      <Url state="input" />
-      <Url state="url" text="website.net" />
-      <File
-        type="folder"
-        title="textddddddddddddddddddddddddddddddd"
-        date="2025.05.05"
-      />
+    <div className="w-full flex flex-col items-center">
+      <div className="flex justify-between">
+        <Navigate state="urls" />
+        <Button colorType="main" iconName="add_circle" />
+      </div>
     </div>
   );
 };
