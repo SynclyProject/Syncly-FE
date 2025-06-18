3953dfc3aced10563eb49c4be07b804ff2ad7659
202110861 <dbsalsk2514@naver.com>
2025-06-18 14:00:13 +0900

    feat: Screen Sharing 참가자 목록

--- Syncly/src/pages/Team/TeamScreenPage.tsx
+++ Syncly/src/pages/Team/TeamScreenPage.tsx
@@ -1,10 +1,14 @@
 import TeamNavigate from "../../components/TeamNavigate";
+import ParticipantsList from "../../components/Screen/ParticipantsList";
 
 const TeamScreenPage = () => {
   return (
-    <div className="w-full mx-[74px] flex flex-col items-center gap-5">
-      <div className="w-full flex mt-5">
+    <div className="w-full h-full mx-[74px] flex flex-col items-center gap-5">
+      <div className="w-full h-full flex flex-col mt-5 gap-5">
         <TeamNavigate state="screen" />
+        <div className="w-full flex flex-col items-center gap-5 bg-[#F7F9FB]">
+          <ParticipantsList />
+        </div>
       </div>
     </div>
   );
