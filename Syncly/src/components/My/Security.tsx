c4e364c240cb182051fe34d0b467b0cddbd030f7
yubal <konsa020318@gmail.com>
2025-07-09 21:14:31 +0900

    refactor: 계정삭제 폼 수정

--- Syncly/src/components/My/Security.tsx
+++ Syncly/src/components/My/Security.tsx
@@ -46,9 +46,9 @@ const Security = () => {
         {/*계정삭제 폼 */}
         
         {showDeleteForm && (
-        <div className="fixed inset-0 z-50  flex items-center justify-center">
+        <div className="fixed inset-0 z-50  flex items-center justify-center ">
           <div className="absolute inset-0 bg-black opacity-40 pointer-events-none" />
-          <div className="relative z-50 bg-white rounded-lg w-96 max-w-full p-6 shadow-lg">
+          <div className="relative z-50 bg-white rounded-lg w-96 max-w-full py-12 px-10 shadow-lg">
             {/* 닫기 버튼 */}
             <button
             
@@ -59,11 +59,12 @@ const Security = () => {
             </button>
 
             {/* 삭제 폼 */}
-            <h2 className="text-center text-rose-500 text-xl font-medium mb-4">
+            <h2 className="text-left text-rose-500 text-xl font-medium mb-4">
               정말로 계정을 삭제하시겠어요?
             </h2>
 
-            <div className="space-y-2 text-sm text-neutral-700 font-thin">
+            <h3 className="text-sm font-medium text-neutral-700 pb-3 ">탈퇴 사유</h3>
+            <div className="space-y-4.5 text-sm text-neutral-700 font-thin">
               {['서비스 이용이 불편해요', '원하는 기능이 없어요', '사용 빈도가 줄었어요', '고객 지원이 만족스럽지 않았어요', '기타'].map(reason => (
                 <label key={reason} className="flex items-center gap-2">
                   <input type="checkbox" className="border rounded" />
@@ -76,6 +77,15 @@ const Security = () => {
                 className="w-full px-3 py-2 border rounded text-xs text-zinc-500"
               />
             </div>
+
+            <div>
+              <h3 className="text-sm font-medium text-neutral-700 pt-4 ">계정 확인</h3>
+              <input
+                type="password"
+                placeholder="비밀번호를 입력해주세요"
+                className="w-full px-3 py-2 border rounded text-xs text-red-400 mt-2"
+              />
+            </div>           
 
 
 
@@ -93,13 +103,6 @@ const Security = () => {
             </div>
              )}
           </div>
-
-
-
-
-
-
-
 
 
       </div>
