4eb2ab4e1af29587f77a1a90070449c1d80b83c9
yubal <konsa020318@gmail.com>
2025-07-23 18:31:16 +0900

    feat: 알람모달 버튼 수정 및 연결

--- Syncly/src/components/alarm/AlarmModal.tsx
+++ Syncly/src/components/alarm/AlarmModal.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Icon from "../../shared/ui/Icon";
+import Button from "../../shared/ui/Button";
 
 interface AlarmModalProps {
   isOpen: boolean;
@@ -10,8 +11,12 @@ const AlarmModal = ({ isOpen, onClose }: AlarmModalProps) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed top-14 right-4 z-50 bg-white shadow-lg rounded-xl p-4 w-[320px]">
+    <div className="fixed top-14 right-3 z-50 bg-white shadow-lg rounded-xl p-4 w-[500px]">
+    
       <h3 className="text-lg font-bold mb-2 flex items-center gap-2"><Icon name="Bell"/> Notification</h3>
+    
+      <hr className="border-t border-gray-200 my-4" />
+
       <div className="space-y-3">
         <div className="flex justify-between items-center bg-gray-50 p-3 rounded-md shadow">
           <p>
@@ -19,12 +24,12 @@ const AlarmModal = ({ isOpen, onClose }: AlarmModalProps) => {
             팀스페이스에서의 초대
           </p>
           <div className="flex gap-2">
-            <button className="bg-[#AEBBFF] text-white px-2 py-1 rounded-md">
+            <Button colorType="sub" className="px-2 py-1 rounded-md">
               수락
-            </button>
-            <button className="border border-gray-300 px-2 py-1 rounded-md">
+            </Button>
+            <Button colorType= "white" className=" px-2 py-1 rounded-md">
               거절
-            </button>
+            </Button>
           </div>
         </div>
         {/* 여러 알림이 있을 수 있음 */}
