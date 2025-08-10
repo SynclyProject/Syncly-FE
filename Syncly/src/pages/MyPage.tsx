cd3089c47670b30e7908c1293fe54cbb2435fe98
202110861 <dbsalsk2514@naver.com>
2025-08-10 15:11:00 +0900

    feat: 회원 정보 조회 API 추가

--- Syncly/src/pages/MyPage.tsx
+++ Syncly/src/pages/MyPage.tsx
@@ -1,20 +1,20 @@
 import Profile from "../components/My/Profile";
 import Security from "../components/My/Security";
+import { useQuery } from "@tanstack/react-query";
+import { GetMemberInfo } from "../shared/api/Member/get_delete";
+
 const MyPage = () => {
+  const { data } = useQuery({
+    queryKey: ["memberInfo"],
+    queryFn: GetMemberInfo,
+  });
+  console.log(data);
+
   return (
     <div className="w-full m-[30px] p-[25px] flex flex-col gap-[30px] bg-white rounded-[8px] border border-[#E0E0E0] shadow-[0px_4px_12px_0px_rgba(0,0,0,0.04)] overflow-auto">
-      <Profile />
-      <Security />
-
-
- 
-
+      <Profile name={data?.result.name} />
+      <Security email={data?.result.email} />
     </div>
-    
-    
-
-
-    
   );
 };
 
