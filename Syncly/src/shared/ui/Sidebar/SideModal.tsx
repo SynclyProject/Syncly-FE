09bf7b54a330f67c337d1e24683e85063bb0de78
202110861 <dbsalsk2514@naver.com>
2025-08-12 14:50:09 +0900

    feat: role 조회 api 연결 코드 추가

--- Syncly/src/shared/ui/Sidebar/SideModal.tsx
+++ Syncly/src/shared/ui/Sidebar/SideModal.tsx
@@ -1,5 +1,8 @@
 import TeamInviteModel from "../../../components/alarm/TeamInviteModel";
 import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { GetSpaceRole } from "../../../shared/api/WorkSpace/get";
+import { useParams } from "react-router-dom";
 
 interface ISideModalProps {
   editTeam: boolean;
@@ -8,6 +11,16 @@ interface ISideModalProps {
 
 const SideModal = ({ editTeam, setEditTeam }: ISideModalProps) => {
   const [showInviteModel, setShowInviteModel] = useState(false);
+
+  const { spaceId } = useParams();
+  const workspaceId = Number(spaceId);
+
+  const { data: roleData } = useQuery({
+    queryKey: ["role"],
+    queryFn: () => GetSpaceRole({ workspaceId: workspaceId }),
+  });
+  console.log("roleData", roleData);
+
   return (
     <div className="flex flex-col gap-5 w-[210px] rounded-[8px] bg-white p-4 border border-[#E0E0E0]">
       <p className="text-[##828282]" onClick={() => setShowInviteModel(true)}>
