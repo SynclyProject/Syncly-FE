c5277b1e3446e05cd7a3848b16461d9a9fb516b1
yubal <konsa020318@gmail.com>
2025-08-29 16:31:34 +0900

    feat: 소셜 수정 중..

--- Syncly/src/shared/api/Social.ts
+++ Syncly/src/shared/api/Social.ts
@@ -1,5 +1,5 @@
 
 export const BeginGoogleLogin = () => {
-  const API_BASE = import.meta.env.VITE_API_URL; // ex) http://localhost:8080
-  window.location.href = `${API_BASE}/oauth2/authorization/google`;
+  const API_BASE = import.meta.env.VITE_API_URL; 
+  window.location.href = `${API_BASE}/api/oauth2/google`;
 };
