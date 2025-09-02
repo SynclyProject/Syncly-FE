1a7036dd5042d34e29c7a83a395921cb4c57a031
yubal <konsa020318@gmail.com>
2025-09-02 16:40:20 +0900

    feat: Social.ts redirect uri 환경변수 분리

--- Syncly/src/shared/api/Social.ts
+++ Syncly/src/shared/api/Social.ts
@@ -1,8 +1,19 @@
-export const BeginGoogleLogin = () => {
+/* export const BeginGoogleLogin = () => {
   const API_BASE = import.meta.env.VITE_API_URL; // 예: https://www.syncly-io.com
   const redirectUri = `${window.location.origin}/oauth2/callback`;
 
   window.location.href = `${API_BASE}/oauth2/authorization/google?redirect_uri=${encodeURIComponent(
     redirectUri
   )}`;
+}; */
+
+export const BeginGoogleLogin = () => {
+  const API_BASE = import.meta.env.VITE_API_URL; 
+  const redirectUri = `${window.location.origin}/oauth2/callback`; 
+  // → 배포 환경에서는 https://www.syncly-io.com/oauth2/callback 이 됨
+
+  window.location.href = `${API_BASE}/oauth2/authorization/google?redirect_uri=${encodeURIComponent(
+    redirectUri
+  )}`;
 };
+
