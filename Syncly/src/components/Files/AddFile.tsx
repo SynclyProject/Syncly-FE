e404d298818c3bd02989b2c41fa38a8757c224e1
202110861 <dbsalsk2514@naver.com>
2025-09-05 14:48:24 +0900

    feat : 파일 추가 컴포넌트에 drag&drop으로 파일 추가하는 코드 추가

--- Syncly/src/components/Files/AddFile.tsx
+++ Syncly/src/components/Files/AddFile.tsx
@@ -1,10 +1,40 @@
 import Icon from "../../shared/ui/Icon";
+import { useState } from "react";
 
 const AddFile = ({
   setAddFileModal,
 }: {
   setAddFileModal: (boolean: boolean) => void;
 }) => {
+  const [isDragging, setIsDragging] = useState(false);
+
+  const handleDragStart = () => {
+    setIsDragging(true);
+  };
+  const handleDragEnd = () => {
+    setIsDragging(false);
+  };
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const files = e.dataTransfer.files[0];
+    console.log(files);
+    setIsDragging(false);
+
+    //드롭된 파일 핸들링
+    handleFileChange(files);
+  };
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement> | File) => {
+    if (e instanceof File) {
+      console.log(e);
+      return;
+    }
+    const files = e.target?.files;
+    console.log(files);
+  };
   return (
     <div className="w-[760px] h-[480px] bg-white flex flex-col gap-8 p-[30px] border border-[#E0E0E0] rounded-[8px]">
       <div className="flex items-center gap-1.5 border-b border-[#828282] py-2">
@@ -19,7 +49,24 @@ const AddFile = ({
           <Icon name="Close" />
         </button>
       </div>
-      <div className="w-full h-full flex items-center justify-center border border-dashed border-[#828282] rounded-[8px]">
+      <div
+        className={`w-full h-full flex items-center justify-center border border-dashed border-[#828282] rounded-[8px]
+      cursor-pointer ${
+        isDragging ? "border-solid border-[#44A257] bg-[#e9e9e9]" : ""
+      }`}
+        onClick={() => document.getElementById("fileInput")?.click()}
+        onDragEnter={handleDragStart}
+        onDragLeave={handleDragEnd}
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
+      >
+        <input
+          type="file"
+          id="fileInput"
+          multiple
+          hidden
+          onChange={handleFileChange}
+        />
         <Icon name="cloud_upload" />
       </div>
     </div>
