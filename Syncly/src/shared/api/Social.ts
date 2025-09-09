7d4a51e3fad994f3c854d6bea1ba6953a79900cc
yubal <konsa020318@gmail.com>
2025-09-09 14:54:01 +0900

    feat: PostReissue 활용

--- Syncly/src/shared/api/Social.ts
+++ Syncly/src/shared/api/Social.ts
@@ -1,15 +1,6 @@
-/* export const BeginGoogleLogin = () => {
-  const API_BASE = import.meta.env.VITE_API_URL; // 예: https://www.syncly-io.com
-  const redirectUri = `${window.location.origin}/oauth2/callback`;
-
-  window.location.href = `${API_BASE}/oauth2/authorization/google?redirect_uri=${encodeURIComponent(
-    redirectUri
-  )}`;
-}; */
-
 export const BeginGoogleLogin = () => {
   const API_BASE = import.meta.env.VITE_API_URL; 
-  const RAW = import.meta.env.VITE_OAUTH_REDIRECT_PATH || "/oauth2/success";
+  const RAW = import.meta.env.VITE_OAUTH_REDIRECT_PATH || "/api/login/oauth2/code/google";
   const REDIRECT_PATH = RAW.startsWith("/") ? RAW : `/${RAW}`;
 
   const redirectUri = `${window.location.origin}${REDIRECT_PATH}`;
