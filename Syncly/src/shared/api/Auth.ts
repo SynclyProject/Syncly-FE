df163272a450f7594dddb726f2219a4776813e5a
202110861 <dbsalsk2514@naver.com>
2025-09-19 15:39:14 +0900

    refactor : 로그인 실패해도 로그인 성공 뜨는 오류 수정

--- Syncly/src/shared/api/Auth.ts
+++ Syncly/src/shared/api/Auth.ts
@@ -1,24 +1,15 @@
 import { axiosInstance, refreshAxios } from "./common/axiosInstance";
 
-
 //로그인
 export const PostLogin = async (data: { email: string; password: string }) => {
-  try {
-    const response = await axiosInstance.post("/api/auth/login", data);
-    return response.data;
-  } catch (error) {
-    console.error("로그인 실패", error);
-  }
+  const response = await axiosInstance.post("/api/auth/login", data);
+  return response.data;
 };
 
 //로그아웃
 export const PostLogout = async () => {
-  try {
-    await axiosInstance.post("/api/auth/logout");
-    console.log("로그아웃 성공");
-  } catch (error) {
-    console.error("로그아웃 실패", error);
-  }
+  await axiosInstance.post("/api/auth/logout");
+  console.log("로그아웃 성공");
 };
 
 //토큰 재발급
