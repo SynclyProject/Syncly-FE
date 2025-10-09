4a6ae01a39aebb5b9ef561537caf781d60ecaeaa
yubal <konsa020318@gmail.com>
2025-10-09 14:04:36 +0900

    feat: MyURLsPage에 skeleton 적용

--- Syncly/src/pages/My/MyURLsPage.tsx
+++ Syncly/src/pages/My/MyURLsPage.tsx
@@ -2,9 +2,23 @@ import Navigate from "../../components/Navigate";
 import URLsList from "../../components/URLs/URLsList";
 import Button from "../../shared/ui/Button";
 import { useState } from "react";
+import MyUrlSkeleton  from "../../shared/ui/Skeleton/MyUrlSkeleton";
+import { useEffect } from "react";
 
 const MyURLsPage = () => {
   const [showInput, setShowInput] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setIsLoading(false), 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+
+  if (isLoading) {
+    return <MyUrlSkeleton />;
+  }
+
   return (
     <div className="w-full mx-[74px] flex flex-col items-center gap-5">
       <div className="w-full flex justify-between mt-5">
