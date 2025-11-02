ca262ee9e0ee2dd1ee688c15a097f6dcba2caab4
202110861 <dbsalsk2514@naver.com>
2025-11-02 13:00:55 +0900

    refactor : 사이드바 위치 코드 수정

--- Syncly/src/shared/ui/Sidebar/Sidebar.tsx
+++ Syncly/src/shared/ui/Sidebar/Sidebar.tsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 const SideBar = () => {
   const [showInput, setShowInput] = useState(false);
   return (
-    <div className="flex flex-col gap-[30px] min-w-[256px] min-h-full h-screen border border-[#E0E0E0] bg-[#F7F9FB] px-[8px]">
+    <div className="sticky top-0 flex flex-col gap-[30px] min-w-[256px] h-screen border border-[#E0E0E0] bg-[#F7F9FB] px-[8px]">
       <Button
         colorType="main"
         iconName="add_circle_outline"
